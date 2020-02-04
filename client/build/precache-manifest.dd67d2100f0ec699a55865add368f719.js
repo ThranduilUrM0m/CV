df3f66fb529ce7c31072b4e2962573d7f8b40e77
Zakariae Boutaleb <zakariaeboutaleb@gmail.com>
2020-02-04 13:54:14 +0100

    right before heroku

---
+++ client/build/precache-manifest.dd67d2100f0ec699a55865add368f719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90b272838f8262c28443c9ce528f368",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f893d2da79a873a5a47",
+    "url": "/static/css/main.d2e5658b.chunk.css"
+  },
+  {
+    "revision": "66a439b28a134d54b17f",
+    "url": "/static/js/2.1a6461bc.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.1a6461bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f893d2da79a873a5a47",
+    "url": "/static/js/main.f4903410.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
