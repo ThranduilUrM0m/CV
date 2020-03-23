1a9476f9aea3bc33886882b2487b7b50543d6de4
Zakariae Bou-taleb <zakariaeboutaleb@gmail.com>
2020-03-23 17:36:41 +0100

    only one form

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -347,9 +347,9 @@ class Home extends React.Component {
                 <Slide>
                     <Form />
                 </Slide>
-                <Slide>
+                {/* <Slide>
                     <Form_Project />
-                </Slide>
+                </Slide> */}
                 <Slide>
                     <section className="active first_section">
                         <div className="wrapper left_part">
