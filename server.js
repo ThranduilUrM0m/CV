56c2d95f3e8ac2d55b277146216fb502a534e4ca
Zakariae Bou-taleb <zakariaeboutaleb@gmail.com>
2020-05-08 19:10:43 +0000

    added cluster, hope it works

--- server.js
+++ server.js
@@ -6,55 +6,117 @@ const cors = require('cors');
 const errorHandler = require('errorhandler');
 const mongoose = require('mongoose');
 
-// IMPORT MODELS
-require('./models/Articles');
-require('./models/Experiences');
-require('./models/Events');
-require('./models/Projects');
-require('./models/Testimonies');
+const cluster = require('cluster');
+let workers = [];
 
-//On définit notre objet express nommé app
-const app = express();
+const setupWorkerProcesses = () => {
+    // to read number of cores on system
+    let numCores = require('os').cpus().length;
+    console.log('Master cluster setting up ' + numCores + ' workers');
 
-//Connexion à la base de donnée
-mongoose.Promise = global.Promise;
-mongoose.set('useFindAndModify', false);
-mongoose
-    .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/db", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
-    .then(() => {
-        console.log("Connected to mongoDB");
-    })
-    .catch((e) => {
-        console.log("Error while DB connecting");
-        console.log(e);
+    // iterate on number of cores need to be utilized by an application
+    // current example will utilize all of them
+    for(let i = 0; i < numCores; i++) {
+        // creating workers and pushing reference in an array
+        // these references can be used to receive messages from workers
+        workers.push(cluster.fork());
+
+        // to receive messages from worker process
+        workers[i].on('message', function(message) {
+            console.log(message);
+        });
+    }
+
+    // process is clustered on a core and process id is assigned
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is listening');
     });
-mongoose.set('debug', true);
 
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cors());
-app.use(require('morgan')('dev'));
-app.use(session({ secret: 'boutaleb', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+    // if any of the worker process dies then start a new one by simply forking another one
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+        workers.push(cluster.fork());
+        // to receive messages from worker process
+        workers[workers.length-1].on('message', function(message) {
+            console.log(message);
+        });
+    });
+};
 
-//Définition du routeur
-const router = express.Router();
-app.use("/user", router);
-require(__dirname + "/controllers/userController")(router);
-app.use(require('./routes'));
+const setUpExpress = () => {
+    // IMPORT MODELS
+    require('./models/Articles');
+    require('./models/Experiences');
+    require('./models/Events');
+    require('./models/Projects');
+    require('./models/Testimonies');
 
-//Définition et mise en place du port d'écoute
-const port = process.env.PORT || 8800;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    //On définit notre objet express nommé app
+    const app = express();
 
-/*Adds the react production build to serve react requests*/
-app.use(express.static(path.join(__dirname, "./client/build")));
+    //Connexion à la base de donnée
+    mongoose.Promise = global.Promise;
+    mongoose.set('useFindAndModify', false);
+    mongoose
+        .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/db", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+        .then(() => {
+            console.log("Connected to mongoDB");
+        })
+        .catch((e) => {
+            console.log("Error while DB connecting");
+            console.log(e);
+        });
+    mongoose.set('debug', true);
 
-/*React root*/
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  
-    const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+    app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(cors());
+    app.use(require('morgan')('dev'));
+    app.use(session({ secret: 'boutaleb', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+    //Définition du routeur
+    const router = express.Router();
+    app.use("/user", router);
+    require(__dirname + "/controllers/userController")(router);
+    app.use(require('./routes'));
+
+    //Définition et mise en place du port d'écoute
+    const port = process.env.PORT || 8800;
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+    /*Adds the react production build to serve react requests*/
+    app.use(express.static(path.join(__dirname, "./client/build")));
+
+    /*React root*/
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('client/build'));
+    
+        const path = require('path');
+        app.get('*', (req,res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        })
+    }
+};
+
+/**
+ * Setup server either with clustering or without it
+ * @param isClusterRequired
+ * @constructor
+ *
+ **/
+
+const setupServer = (isClusterRequired) => {
+
+    // if it is a master process then call setting up worker process
+    if(isClusterRequired && cluster.isMaster) {
+        setupWorkerProcesses();
+    } else {
+        // to setup server configurations and share port address for incoming requests
+        setUpExpress();
+    }
+};
+
+setupServer(true);
