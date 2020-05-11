e61e4be763ffd07f1200612bc39fbefafe00cf43
Zakariae Bou-taleb <zakariaeboutaleb@gmail.com>
2020-05-11 17:55:58 +0000

    Search Added

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import logo from '../../logo.svg';
 import favicon from '../../favicon.svg';
 import API from '../../utils/API';
@@ -12,13 +15,52 @@ class Header extends React.Component {
     constructor(props) {
         super(props);
 		this.state = {
-			logo_to_show: logo,
+            logo_to_show: logo,
+            _search_value: '',
+            currentPage: 1,
+          	todosPerPage: 4,
+			currentCard: 0,
 		};
         this._handleClickEvents = this._handleClickEvents.bind(this);
+        this._handleClickPage = this._handleClickPage.bind(this);
+        this.handleJSONTOHTMLIMAGE = this.handleJSONTOHTMLIMAGE.bind(this);
+		this._FormatNumberLength = this._FormatNumberLength.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
     componentDidMount() {
+        const { onLoad, onLoadProject } = this.props;
+
         this._handleClickEvents();
+        this.handleSearch();
         const self = this;
+
+        axios('/api/articles')
+        .then(function (response) {
+            // handle success
+            onLoad(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+
+        axios('/api/projects')
+        .then(function (response) {
+            // handle success
+            onLoadProject(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+
         let _links = $('.menu .nav-link');
         let _url = window.location.pathname;
         _links.map((_link, index) => {
@@ -103,7 +145,9 @@ class Header extends React.Component {
         let searchWrapper = document.querySelector('.search-wrapper'),
             searchInput = document.querySelector('.search-input'),
             searchIcon = document.querySelector('.search'),
-            searchActivated = false;
+            $searchModal = $('#search-modal'),
+            searchActivated = false,
+            self = this;
 
         $('.search_form').click((event) => {
             if (!searchActivated) {
@@ -118,6 +162,10 @@ class Header extends React.Component {
                     searchIcon.classList.remove('active');
                     searchActivated = !searchActivated;
                     $('.overlay_menu').toggleClass('overlay_menu--is-closed');
+                    $searchModal.removeClass('active');
+                    self.setState({
+                        _search_value: ''
+                    });
                 }
             }
         });
@@ -219,8 +267,45 @@ class Header extends React.Component {
         };
         });
     }
+	_handleClickPage(event) {
+        this.setState({
+          	currentPage: Number(event.target.id)
+        });
+	}
+	handleJSONTOHTMLIMAGE(inputDelta, index) {
+		function runAfterElementExists(jquery_selector, callback){
+			var checker = window.setInterval(function() {
+			if (jquery_selector) {
+				clearInterval(checker);
+				callback();
+			}}, 200);
+		}
+		runAfterElementExists(inputDelta, function() {
+			const html = $.parseHTML(inputDelta);
+			$('.card_'+index+' figure').html($(html).find('img').first());
+		});
+	}
+	_FormatNumberLength(num, length) {
+		var r = "" + num;
+		while (r.length < length) {
+			r = "0" + r;
+		}
+		return r;
+	}
+    handleSearch() {
+        let self = this;
+        $('.search-input').on('focusin', function() {
+            $('#search-modal').addClass('active');
+        });
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
     render() {
-        const { logo_to_show } = this.state;
+        const { logo_to_show, _search_value, currentPage, todosPerPage } = this.state;
+        const { articles, projects } = this.props;
         return (
             <>
                 <div className="overlay_menu overlay_menu--is-closed"></div>
@@ -239,13 +324,19 @@ class Header extends React.Component {
                             <a className="logoHolder" href="/">
                                 <img className="logo img-fluid" src={logo_to_show} alt="Risala"/>
                             </a>
-                            {/* <div className="js-lang u-mb-15">
-                                <span className="js-fr">fr</span>
-                                <span className="js-en is-active">en</span>
-                            </div> */}
                             <form className="search_form">
-                                <div className="search-wrapper">
-                                    <input className="search-input" type="text" placeholder="Search"/>
+                                <div className="input-field search-wrapper">
+                                    <input 
+                                        className="search-input validate form-group-input _search_value"
+                                        id="_search_value"
+                                        type="text"
+                                        name="_search_value"
+                                        value={_search_value} 
+                                        onChange={this.handleChange}
+                                    />
+                                    <label htmlFor='_search_value' className={_search_value ? 'active' : ''}>Search.</label>
+                                    <div className="form-group-line"></div>
+
                                     <span className="hover_effect"></span>
                                     <div className='search'></div>
                                 </div>
@@ -254,6 +345,83 @@ class Header extends React.Component {
                     </div>
                 </div>
                 {/* the actual header */}
+                <div id="search-modal">
+                    <div className="search-modal-background"></div>
+                    <div className="search-content-area">
+                        <div className="search-results">
+                            <ul id="page">
+                                {
+                                    _.slice(_.filter(_.filter(articles, (_a) => { return !_a._hide && _search_value != '' }), (_article) => {
+                                        return _.includes(_article.title, _search_value) || _.includes(_article.author, _search_value) || _.includes(_article.categorie, _search_value) || _article.tag.some(x => x.includes(_search_value));
+                                    }), ((currentPage * todosPerPage) - todosPerPage), (currentPage * todosPerPage)).map((article, index) => {
+                                        return (
+                                            <li className="article_card article_anchor" data-name={ moment(article.createdAt).format("YYYY Do MM") } id="article_card" key={index}>
+                                                <div className={"col card card_" + index} data-title={_.snakeCase(article.title)} data-index={_.add(index,1)}>
+                                                    <div className="card-body">
+                                                        <div className="text">
+                                                            <p className="text-muted author">by <b>{article.author}</b>, {moment(new Date(article.createdAt)).fromNow()}</p>
+                                                            <figure>{this.handleJSONTOHTMLIMAGE(article.body, index)}</figure>
+                                                            <h4>{article.title}</h4>
+                                                            <Link to={`blog/${article._id}`}>
+                                                                <div className="readmore">
+                                                                    <button data-am-linearrow="tooltip tooltip-bottom" display-name="Read More">
+                                                                        <div className="line line-1"></div>
+                                                                        <div className="line line-2"></div>
+                                                                    </button>
+                                                                </div>
+                                                            </Link>
+                                                            <div className="comments_up_down">
+                                                                <p className="text-muted views"><b>{_.size(article.view)}</b><i className="fas fa-eye"></i></p>
+                                                                <p className="text-muted comments"><b>{_.size(article.comment)}</b> <i className="fas fa-comment-alt"></i></p>
+                                                                <p className="text-muted upvotes"><b>{_.size(article.upvotes)}</b> <i className="fas fa-thumbs-up"></i></p>
+                                                                <p className="text-muted downvotes"><b>{_.size(article.downvotes)}</b> <i className="fas fa-thumbs-down"></i></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                            <ul id="page-numbers">
+                                {
+                                    ([...Array(Math.ceil(_.filter(_.filter(articles, (_a) => { return !_a._hide && _search_value != '' }), (_article) => {
+                                        return _.includes(_article.title, _search_value) || _.includes(_article.author, _search_value) || _.includes(_article.categorie, _search_value) || _article.tag.some(x => x.includes(_search_value));
+                                    }).length / todosPerPage)).keys()]).map(number => {
+                                        return (
+                                            <li
+                                                key={number+1}
+                                                id={number+1}
+                                                onClick={this._handleClickPage}
+                                                className={currentPage === number+1 ? 'current' : ''}
+                                                >
+                                                    <p className="shadow_page">.{this._FormatNumberLength(number+1,2)}</p>
+                                            </li>
+                                        );
+                                    })
+                                }
+                            </ul>
+                        </div>
+                        <div className="search-suggestions">
+                            <div><small>Here</small></div>
+                            <div className="togglebtn">👉 May we suggest?</div>
+                            <ul className="text-muted tags">
+                                {
+                                    _.orderBy(_.filter(articles, (_a) => { return !_a._hide }), ['comment', 'upvotes', 'view'], ['desc', 'desc', 'desc']).map((_article, _index) => {
+                                        return (
+                                            _article.tag.map((t, i) => {
+                                                return (
+                                                    <li className="tag_item" onClick={() => { this.setState({_search_value: t}); }}>{t}</li>
+                                                )
+                                            })
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 <ul className="menu menu--is-closed">
                     <li><span className="item item-0"></span></li>
                     <li><span className="item item-1"><Link to='/' className="nav-link" id="_home_link"> Home. </Link></span></li>
@@ -262,21 +430,19 @@ class Header extends React.Component {
                     <li><span className="item item-4"><Link to='/minttea' className="nav-link" id="_minttea_link"> Mint Tea. </Link></span></li>
                     <li><span className="item item-5"><Link to='/contact' className="nav-link" id="_contact_link"> Contact Me. </Link></span></li>
                 </ul>
-                <div className="modal fade" id="signup_modal" tabIndex="-1" role="dialog" aria-labelledby="signup_modalLabel" aria-hidden="true">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-body">
-                                <a title="Close" className="modal-close" data-dismiss="modal">Close</a>
-                                <h5 className="modal-title" id="signup_modalLabel">Welcome!</h5>
-                                <div>We have sent you a verification email, all you have to do is just click it and boom you are one of us now.</div>
-                                <div><small>Welcome {localStorage.getItem('username')}</small></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </>
         );
     }
 }
 
-export default Header;
+const mapStateToProps = state => ({
+    articles: state.home.articles,
+    projects: state.home.projects,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onLoad: data => dispatch({ type: 'HOME_PAGE_LOADED', data }),
+    onLoadProject: data => dispatch({ type: 'PROJECT_PAGE_LOADED', data }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
